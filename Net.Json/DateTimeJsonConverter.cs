da12b5fa45a19974ac6120e5108ba024c2016776
Salih <keles.salih@gmail.com>
2020-01-06 18:33:19 +0300

    datetime serializer changed

--- Net.Json/DateTimeJsonConverter.cs
+++ Net.Json/DateTimeJsonConverter.cs
@@ -6,31 +6,31 @@ namespace Net.Json
     public class DateTimeJsonConverter : JsonConverter
     {
         public static readonly DateTimeJsonConverter Default = new DateTimeJsonConverter();
-
+        static DateTime MinDate = new DateTime(1902, 1, 1, 0, 0, 0, DateTimeKind.Utc);
         public override bool CanConvert(Type objectType)
         {
             return objectType == typeof(DateTime) || objectType==typeof(DateTime?) ;
         }
 
-        public override bool CanWrite => true;
+        public override bool CanWrite => false;
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             if (reader.Value == null)
             {
-                if (objectType == typeof(DateTime)) return DateTime.MinValue;
+                if (objectType == typeof(DateTime)) return MinDate;
                 return  null;
             }
             if(reader.ValueType==typeof(string))
             {
                 if (DateTime.TryParse(reader.Value as string, out DateTime result)) return result;
-                if(objectType==typeof(DateTime)) return DateTime.MinValue;
+                if(objectType==typeof(DateTime)) return MinDate;
                 return null;
             }
             if (reader.ValueType == typeof(DateTime)) return reader.Value;
             var val = Convert.ToInt64(reader.Value);
             var netticks = 10000 * val + 621355968000000000;
-            netticks = Math.Max(DateTime.MinValue.Ticks, netticks);
+            netticks = Math.Max(MinDate.Ticks, netticks);
             netticks = Math.Min(DateTime.MaxValue.Ticks, netticks);
             return new DateTime(netticks,DateTimeKind.Utc).ToLocalTime();
         }
