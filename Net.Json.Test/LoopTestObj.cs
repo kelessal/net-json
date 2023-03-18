001dad5799830f41ca85bdb43ac9fdb3d0278130
Salih <keles.salih@gmail.com>
2023-03-19 01:13:06 +0300

    Upgrade pack

--- Net.Json.Test/LoopTestObj.cs
+++ Net.Json.Test/LoopTestObj.cs
@@ -9,4 +9,33 @@ namespace Net.Json.Test
         public string Name { get; set; }
         public LoopTestObj LoopProp { get; set; }
     }
+    public interface IRequestInfo
+    {
+        string Token { get; set; }
+        string TransactionId { get; }
+        string EndPoint { get; }
+        string Language { get; }
+        string IPAddress { get; }
+        string Currency { get; }
+        string AppId { get; }
+        string AccountId { get; }
+        string HotelId { get; }
+        string ReservationId { get; }
+        string DeviceId { get; }
+        bool Force { get; set; }
+        string UserAccountId { get; }
+        DateTime RequestDate { get; }
+        long RequestDay { get; }
+        TimeSpan TimeOffset { get; }
+        string Host { get; }
+        string Referer { get; }
+        
+      
+        bool IsTest { get; }
+        bool AllLangs { get; set; }
+        TimeRange StaffActiveTimeRange { get; set; }
+        string TokenIPAddress { get; set; }
+        bool TokenExpired { get; set; }
+       
+    }
 }
--- Net.Json.Test/SerializationTest.cs
+++ Net.Json.Test/SerializationTest.cs
@@ -58,5 +58,12 @@ namespace Net.Json.Test
             dynamic expando = obj.AsExpandoObject();
             var list = expando.list;
         }
+
+        [Fact] 
+        public void IntTest()
+        {
+            var x= "{'token':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjlmYzM2NWRhLTlhNGYtNGVjMS1hYzczLTJkMmE2OTg3OWRkYiIsIm5hbWVpZCI6IjlmYzM2NWRhLTlhNGYtNGVjMS1hYzczLTJkMmE2OTg3OWRkYiIsImFpZCI6ImU2MmZlZDYxLTk5MTEtNDg3OS04YzQxLWExYzI3ZjU3ZGNkOCIsImNuYW1lIjoiU2FsaWggS2VsZcWfIiwiYXV0aCI6IjMiLCJqYWlkIjoiMDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAwIiwicGljIjoiN2Y3OWE5NTAtYTcwYi00ZTNiLWIxYzUtNGVhMjZkOTAyYjgwIiwidW5hbWUiOiJzYWxpaGtlbGVzIiwidXNpZCI6IjlmYzM2NWRhLTlhNGYtNGVjMS1hYzczLTJkMmE2OTg3OWRkYiIsInVzYWNpZCI6IjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMCIsImhvc3QiOiIiLCJpcCI6IjEwLjAuMzAuNDUiLCJhY3RpdmUtc3RhcnQtdGltZSI6IjAwOjAwOjAwIiwiYWN0aXZlLWVuZC10aW1lIjoiMS4wMDowMDowMCIsIm5iZiI6MTY3Nzg1OTk1NCwiZXhwIjoxNjc3ODYwNTU0LCJpYXQiOjE2Nzc4NTk5NTQsImlzcyI6ImJpLnRlY2hub2xvZ3kuYWNjb3VudC5zZXJ2aWNlIn0.MnTu00RocSUEEhOxcTmYZo4JfOiIcJfehpmLtF-laBE','authType':3,'language':'en','currency':'usd','iPAddress':'127.0.0.1','requestPath':'/api/application/application/info','endPoint':'custom:get:application/info','appId':'00000000-0000-0000-0000-000000000000','accountId':'e62fed61-9911-4879-8c41-a1c27f57dcd8','hotelId':'70073f67-b56a-41f0-8b8a-add7c679debc','force':false,'deviceId':'1669375995036-25779019339','rTokenExists':false,'tokenExists':true,'availableHotelIds':null,'availableCurrencies':null,'availableLanguages':null,'identity':{'name':'Salih Keleş','picture':'7f79a950-a70b-4e3b-b1c5-4ea26d902b80','accountId':null,'id':'9fc365da-9a4f-4ec1-ac73-2d2a69879ddb'},'userAccountId':'00000000-0000-0000-0000-000000000000','requestDate':'2023-03-03T16:21:41.2436831Z','requestDay':20230303,'timeOffset':10800000,'host':'admin.bitechnology.com','isAdministrator':false,'reservationId':null,'transactionId':'909GOlM4J0CInMaEC_7eHA','isAdminStaff':false,'referer':'http://admin.bitechnology.com/','isTest':false,'allLangs':false,'staffActiveTimeRange':{'start':0,'end':86400000},'tokenIPAddress':null,'tokenExpired':false}";
+           var c= x.Deserialize<IRequestInfo>();
+        }
     }
 }
